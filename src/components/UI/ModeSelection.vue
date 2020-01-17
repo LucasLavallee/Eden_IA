d1f28a91cfada48dda4dfd3633ef000ef6401701
Jeremyrts <jeremyrts@Pumba.local>
2020-01-17 13:52:42 +0100

    Change in interface

--- src/components/UI/ModeSelection.vue
+++ src/components/UI/ModeSelection.vue
@@ -1,4 +1,6 @@
 <template>
+  <div id="container">
+    <span>Tools</span>
     <div id="modeSelection">
         <div class="mode" :class="getCurrentMode === 'navigate' ? 'active': ''" @click="changeCurrentMode('navigate')">
             <p>Navigate</p>
@@ -6,16 +8,34 @@
         <div class="mode" :class="getCurrentMode === 'add' ? 'active': ''" @click="changeCurrentMode('add')">
             <p>Add</p>
         </div>
-        <div v-if="getCurrentMode === 'add'" class="items-container">
-              <div class="item" id="carot" :class="getCurrentSelection === 'carot' ? 'active': ''" @click="changeCurrentSelection('carot')"> Carot </div>
-              <div class="item" id="banana" :class="getCurrentSelection === 'banana' ? 'active': ''" @click="changeCurrentSelection('banana')"> Banana </div>
-              <div class="item" id="pear" :class="getCurrentSelection === 'pear' ? 'active': ''" @click="changeCurrentSelection('pear')"> Pear </div>
-              <div class="item" id="tomato" :class="getCurrentSelection === 'tomato' ? 'active': ''" @click="changeCurrentSelection('tomato')"> Tomato </div>
-        </div>
         <div class="mode" :class="getCurrentMode === 'remove' ? 'active': ''" @click="changeCurrentMode('remove')">
             <p>Remove</p>
         </div>
+        <transition
+          name="fade"
+          v-on:before-enter="beforeEnter"
+          v-on:enter="enter"
+          v-on:after-enter="afterEnter"
+          v-on:enter-cancelled="enterCancelled"
+
+          v-on:before-leave="beforeLeave"
+          v-on:leave="leave"
+          v-on:after-leave="afterLeave"
+          v-on:leave-cancelled="leaveCancelled"
+        >
+          <div v-if="getCurrentMode === 'add'" class="items-container">
+            <div class="item" id="carot" :class="getCurrentSelection === 'carot' ? 'active': ''" @click="changeCurrentSelection('carot')"> C </div>
+            <div class="item" id="banana" :class="getCurrentSelection === 'banana' ? 'active': ''" @click="changeCurrentSelection('banana')"> B </div>
+            <div class="item" id="pear" :class="getCurrentSelection === 'pear' ? 'active': ''" @click="changeCurrentSelection('pear')"> P </div>
+            <div class="item" id="tomato" :class="getCurrentSelection === 'tomato' ? 'active': ''" @click="changeCurrentSelection('tomato')"> T </div>
+            <div class="item"> XX </div>
+            <div class="item"> XX </div>
+            <div class="item"> XX </div>
+            <div class="item"> XX </div>
+          </div>
+        </transition>
     </div>
+  </div>
 </template>
 
 <script>
@@ -44,12 +64,19 @@ export default {
 </script>
 
 <style lang="stylus">
+  #container
+    position fixed
+    top 20px
+    left 20px
+    font-size 20px
     #modeSelection
-      position fixed
-      top 20px
+      // position fixed
+      // top 20px
       left 20px
       display flex
       flex-direction column
+      width 200px
+      font-size 0.8em
 
     .mode
       margin 5px 0
@@ -63,13 +90,20 @@ export default {
     .items-container
       display flex
       flex-direction row
+      border solid 2px yellow
+      justify-content center
+      margin-top 20px
+      flex-wrap wrap
 
       .item
         background-color #fff
         margin 10px
         padding 5px
         cursor pointer
+        width 20px
+        height 20px
 
         &.active
           background-color blue
+
 </style>
