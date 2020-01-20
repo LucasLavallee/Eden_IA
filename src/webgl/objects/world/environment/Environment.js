ae818bc27d7aa69617fb1a52a261fa4c987ccd21
Jeremyrts <jeremyrts@Pumba.local>
2020-01-20 15:06:28 +0100

    Remove datGUI pannel from the UI

--- src/webgl/objects/world/environment/Environment.js
+++ src/webgl/objects/world/environment/Environment.js
@@ -1,4 +1,4 @@
-import { datGui } from 'utils/debug'
+// import { datGui } from 'utils/debug'
 
 export default class Environment {
   constructor () {
@@ -9,20 +9,20 @@ export default class Environment {
       pollution: 0
     }
 
-    this.addEnvironmentGui()
+    // this.addEnvironmentGui()
   }
 
   init () {
 
   }
 
-  addEnvironmentGui () {
-    const environmentFolder = datGui.addFolder('Environment properties')
-    environmentFolder.add(this.properties, 'temperature').min(0).max(40).step(1)
-    environmentFolder.add(this.properties, 'humidity').min(0).max(100).step(1)
-    environmentFolder.add(this.properties, 'brightness').min(0).max(100).step(1)
-    environmentFolder.add(this.properties, 'pollution').min(0).max(100).step(1)
-  }
+  // addEnvironmentGui () {
+  //   const environmentFolder = datGui.addFolder('Environment properties')
+  //   environmentFolder.add(this.properties, 'temperature').min(0).max(40).step(1)
+  //   environmentFolder.add(this.properties, 'humidity').min(0).max(100).step(1)
+  //   environmentFolder.add(this.properties, 'brightness').min(0).max(100).step(1)
+  //   environmentFolder.add(this.properties, 'pollution').min(0).max(100).step(1)
+  // }
 
   getProperties () {
     return this.properties
